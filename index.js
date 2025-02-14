063d3b02cc1809d197d25003955311225cf5b31e
Shiva Sunchu <shivasunchu06@gmail.com>
2025-02-14 16:53:01 +0530

    adding day-1

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const application = express();
+const dotenv = require("dotenv");
+const { userRouter } = require("./routes/user.route");
 
+dotenv.config();
+const application = express();
 application.use(express.json());
+application.use("/api", userRouter);
 
 application.get("/", (request, response) => {
   response.send("Hello World!");
---
+++ models/user.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: false, // optional field
+    },
+    googleId: {
+      type: String,
+      required: false, // optional field
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true, // Adds createdAt and updatedAt fields automatically
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
