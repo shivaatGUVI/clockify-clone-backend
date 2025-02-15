e7bac6a0263d91a617b7a1f2a4a19c14a0e74067
Shiva Sunchu <shivasunchu06@gmail.com>
2025-02-15 13:56:23 +0530

    adding day-2

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const tracker = require("./routes/tracker");
 const categories = require("./routes/category");
 const userRoute = require("./routes/userRoute");
 const groupRoutes = require("./routes/groupRoutes");
 const { userRouter } = require("./routes/user.route");
+const { connectToMongoDB } = require("./utils/db");
 
 dotenv.config();
 const application = express();
 
-// testing purposes
-mongoose
-  .connect("mongodb://localhost:27017/clockifyClone")
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Could not connect to MongoDB:", err));
-
 application.use(express.json());
+application.use(cors());
 application.use("/api", userRouter);
 application.use("/tracker", tracker);
 application.use("/categories", categories);
@@ -27,6 +24,11 @@ application.get("/", (request, response) => {
   response.send("Hello World!");
 });
 
-application.listen(8080, () => {
-  console.log("Server started");
+application.listen(8080, async () => {
+  console.log("Server is running on port 8080");
+  try {
+    await connectToMongoDB();
+  } catch (err) {
+    console.log(err);
+  }
 });
